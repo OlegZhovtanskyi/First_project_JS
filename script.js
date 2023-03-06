e00816e9722108a22ea16af5bb0e734da8ee41ec
Oleg <o.zhovtanskyi@gmail.com>
2023-03-06 14:53:55 +0100

    The program has been redesigned, adding a loop and if statements for faster operation and code reduction.

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ let personalMovieDB = {
 	private: false,
 };
 
-const a = prompt("The name of the movie you are watching last", ""),
-	b = +prompt("What rating would you give this movie?", ""),
-	c = prompt("The name of the movie you are watching last", ""),
-	d = +prompt("What rating would you give this movie?", "");
+for (let i = 0; i < 2; i++) {
+	const a = prompt("The name of the movie you are watching last", ""),
+		b = +prompt("What rating would you give this movie?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+	if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log("done");
+	} else {
+		console.log("error");
+		i--;
+	}
+}
+
+if (personalMovieDB.count <= 30) {
+	console.log(
+		"You've seen very few movies in your life"
+	);
+} else if (personalMovieDB.count <= 70) {
+	console.log("Congratulations, you are an average statistical person");
+} else if (personalMovieDB.count > 70) {
+	console.log("Yo dude you are legend");
+} else {
+	console.log("error");
+}
 
 console.log(personalMovieDB);
