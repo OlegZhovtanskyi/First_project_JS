b27c29b58ac8cf93110c96afdcdaa1983ee0fd89
Oleg <o.zhovtanskyi@gmail.com>
2023-03-08 10:23:28 +0100

    The program was rewritten using functions to make it faster

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const numberOfMovies = +prompt("How many movies you already watched?", "");
-
-let personalMovieDB = {
+let numberOfMovies;
+start();
+const personalMovieDB = {
 	count: numberOfMovies,
 	movies: {},
 	actors: {},
@@ -8,29 +8,64 @@ let personalMovieDB = {
 	private: false,
 };
 
-for (let i = 0; i < 2; i++) {
-	const a = prompt("The name of the movie you are watching last", ""),
-		b = +prompt("What rating would you give this movie?", "");
+function start() {
+	numberOfMovies = +prompt("How many movies you already watched?", "");
 
-	if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-		personalMovieDB.movies[a] = b;
-		console.log("done");
-	} else {
-		console.log("error");
-		i--;
+	while (
+		numberOfMovies == "" ||
+		numberOfMovies == null ||
+		isNaN(numberOfMovies)
+	) {
+		numberOfMovies = +prompt("How many movies you already watched?", "");
 	}
 }
 
-if (personalMovieDB.count <= 30) {
-	console.log(
-		"You've seen very few movies in your life"
-	);
-} else if (personalMovieDB.count <= 70) {
-	console.log("Congratulations, you are an average statistical person");
-} else if (personalMovieDB.count > 70) {
-	console.log("Yo dude you are legend");
-} else {
-	console.log("error");
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt("The name of the movie you are watching last", ""),
+			b = +prompt("What rating would you give this movie?", "");
+
+		if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+		} else {
+			console.log("error");
+			i--;
+		}
+	}
 }
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+	if (personalMovieDB.count <= 30) {
+		console.log("You've seen very few movies in your life");
+	} else if (personalMovieDB.count <= 70) {
+		console.log("Congratulations, you are an average statistical person");
+	} else if (personalMovieDB.count > 70) {
+		console.log("Yo dude you are legend");
+	} else {
+		console.log("error");
+	}
+}
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	} else if (hidden) {
+		console.log("You don't have access to this object");
+	} else {
+		console.log("Error");
+	}
+}
+
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt(
+			`You favorite movies genre: ${i}`
+		);
+	}
+}
+
+
+rememberMyFilms();
+detectPersonalLevel();
+showMyDB(personalMovieDB.private);
+writeYourGenres();
